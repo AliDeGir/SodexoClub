5c9a9803a958a987dd6bb3d7ffe3b129672e4b2d
AliDeGir <ali_deg@hotmail.com>
2024-03-31 23:00:15 +0200

    contact downloaded and displayed

---
+++ src/firebase/modules/fetchContact.js
@@ -0,0 +1,117 @@
+import { db } from "../config";
+import { useState, useEffect } from "react";
+import { getDocs, collection, doc, getDoc } from "firebase/firestore";
+
+const FetchContact = () => {
+  const [contactData, setContactData] = useState([]);
+  const [photoUrls, setPhotoUrls] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetching contact data
+        const contactPaths = [
+          "messagesApp/contactDb/contact/Ali Degirmenci",
+          "messagesApp/contactDb/contact/Marina Fedjajeva",
+          "messagesApp/contactDb/contact/Salwa Zakaria",
+        ];
+
+        const contactPromises = contactPaths.map((path) =>
+          getDoc(doc(db, path)).then((docSnapshot) => ({
+            id: docSnapshot.id,
+            data: docSnapshot.data(),
+          }))
+        );
+
+        const contactResults = await Promise.all(contactPromises);
+        setContactData(contactResults);
+
+        // Fetching photo URLs
+        const photoPaths = [
+          "messagesApp/photosDb/photos/raoe5QY36fWZgHVsg0qZ",
+          "messagesApp/photosDb/photos/ZjNylCkih0dUb0lhrRas",
+        ];
+
+        const photoPromises = photoPaths.map((path) =>
+          getDoc(doc(db, path)).then((docSnapshot) =>
+            docSnapshot.exists ? docSnapshot.data().url : null
+          )
+        );
+
+        const photoUrls = await Promise.all(photoPromises);
+        setPhotoUrls(photoUrls);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="contact-div">
+      <div className="contact-card">
+        <div className="contact-photo">
+          {photoUrls[0] ? (
+            <img src={photoUrls[0]} alt={`Photo 1`} />
+          ) : (
+            "No photo"
+          )}
+        </div>
+        <div className="contact-info">
+          <h4>{contactData[0] && contactData[0].id}</h4>
+          <p>
+            {contactData[0] && contactData[0].data.title}
+            <br />
+            {contactData[0] && contactData[0].data.phone}
+            <br />
+            {contactData[0] && contactData[0].data.email}
+            <br />
+            {contactData[0] && contactData[0].data.links}
+            <br />
+          </p>
+        </div>
+      </div>
+      <div className="contact-card">
+        <div className="contact-photo">No photo</div>
+        <div className="contact-info">
+          <h4>{contactData[2] && contactData[2].id}</h4>
+          <p>
+            {contactData[2] && contactData[2].data.title}
+            <br />
+            {contactData[2] && contactData[2].data.phone}
+            <br />
+            {contactData[2] && contactData[2].data.email}
+            <br />
+            {contactData[2] && contactData[2].data.links}
+            <br />
+          </p>
+        </div>
+      </div>
+      <div className="contact-card">
+        <div className="contact-photo">
+          {photoUrls[1] ? (
+            <img src={photoUrls[1]} alt={`Photo 2`} />
+          ) : (
+            "No photo"
+          )}
+        </div>
+        <div className="contact-info">
+          <h4>{contactData[1] && contactData[1].id}</h4>
+          <p>
+            {contactData[1] && contactData[1].data.title}
+            <br />
+            {contactData[1] && contactData[1].data.phone}
+            <br />
+            {contactData[1] && contactData[1].data.email}
+            <br />
+            {contactData[1] && contactData[1].data.links}
+            <br />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FetchContact;
--- src/newBuild/contact/Contact.js
+++ src/newBuild/contact/Contact.js
@@ -1,10 +1,12 @@
 import React from "react";
 import FetchList from "../../firebase/modules/fetchListModule";
+import FetchContact from "../../firebase/modules/fetchContact";
 
 const Contact = () => {
     return (
         <>
             <div>Contact</div>
+            <FetchContact />
             {/* <FetchList collectionPath="messagesApp/landingPageDb/landingPage" /> */}
         </>
     );
